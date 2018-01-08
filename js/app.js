d89fdf47120c921cbb0134f8e1061f8a55f74da3
romina2911 <romina.hp@gmail.com>
2018-01-07 20:43:15 -0500

    subiendo mi jquery

--- js/app.js
+++ js/app.js
@@ -1,76 +1,38 @@
-window.addEventListener('load', function() {
-  var tweetArea = document.getElementById('tweet-area');
-  var tweetBtn = document.getElementById('tweet-btn');
-  var messages = document.getElementById('messages');
-  var countDinamic = document.getElementById('count');
-  var MAXCHARACTERS = 140;
-
-  tweetArea.addEventListener('keyup', function(event) {
-    console.log(event.target.value.trim().length);
-    if (event.target.value.trim().length) {
-      var total = MAXCHARACTERS - event.target.value.trim().length;
-      tweetBtn.disabled = false;
-      countDinamic.textContent = MAXCHARACTERS - event.target.value.trim().length;
-      console.log(event.target.value.trim());
-      if (event.target.value.trim().length > MAXCHARACTERS) {
-        tweetBtn.disabled = true;
+$(document).ready(function() {
+  var textArea = $('#tweet-area');
+  // var textArea = document.querySelector('#tweet-area');
+  var button = $('#tweet-btn');
+  var msg = $('#messages');
+  var contador = $('#count');
+  var MAXCOUNT = 140;
+  
+  textArea.on('input', function(event) { //funcion manejadora de eventos para la var
+  // console.log((event.target.value));
+  // console.log((($(this)).val()).length);
+  var long = $(this).val().length;
+  contador.text(MAXCOUNT-long);
+  if(long>0) {
+      button.attr('disabled', false);
+      if(long > 120) {
+        contador.css('color', 'green');
+      } else{
+          contador.css('color', 'black');
       }
-      countDinamic.classList.toggle('seagreen', 10 <= total && total < 20);
-      countDinamic.classList.toggle('orangered', total < 10);
-    } else {
-      tweetBtn.disabled = true;
-      countDinamic.textContent = MAXCHARACTERS;
-      // console.log('la cadena esta vacia'); 
-    }
-    // Versión 0.0.4
-    var text = event.target.value.split('');
-    var acum = 0;
-    for (var i = 0; i < text.length; i++) {
-      if (text[i] === '\n') {
-        acum++;
+      if (long > 130) {
+         contador.css('color', 'red');
+      } if (long > 140) {
+          button.attr('disabled', true);
       }
-      if (acum) {
-        event.target.rows = acum + 2;
-      }
-    }
-    // Versión 0.0.5 (Extra)
-    if ((event.target.value.trim().length / event.target.cols) < event.target.rows) {
-      event.target.rows = (event.target.value.trim().length / event.target.cols) + 2;
-    }
+  }else{
+      button.attr('disabled', true);
+  }
+  // if(long === 20){
+  //     ($(this)).css('color', 'pink');
+  //     button.attr('disabled', false);
+  });
+  button.on('click', function(event){
+      event.preventDefault();
+      msg.prepend('<div>'+textArea.val()+'</div>').css({'background-color':'white', 'color':'red'});
   });
   
-  tweetArea.addEventListener('keydown', function(event) {
-    countDinamic.textContent = MAXCHARACTERS - tweetArea.value.length;
   });
-
-  tweetBtn.addEventListener('click', function(event) {
-    event.preventDefault();
-    // console.log(tweetArea.value);
-    // console.log(tweetArea.value.lenght);
-    var div = document.createElement('div');
-    var tweet = document.createElement('span');
-    var tweetText = document.createTextNode(tweetArea.value);
-    
-    // Versión 0.0.6 (Extra)
-    /* 
-    var fecha = new Date();
-    var hora = fecha.getHours();
-    var minutos = fecha.getMinutes();
-    */
-    var hours = moment().format('LT');
-    var fechaCompleta = document.createTextNode(hours);
-    var espacio = document.createTextNode(' ');
-
-    tweet.appendChild(tweetText);
-    tweet.appendChild(espacio);
-    tweet.appendChild(fechaCompleta);
-    div.appendChild(tweet);
-    div.classList.add('nuevo-mensaje');
-    messages.insertBefore(div, messages.firstElementChild);
-
-    tweetArea.value = '';
-    tweetArea.focus();
-    tweetBtn.disabled = true;
-    countDinamic.textContent = MAXCHARACTERS;
-  });
-});
